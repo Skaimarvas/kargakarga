fdc452a2eae33f4fb965bfe1e3b5287419c8993b
Kaim Arvas <skaimarvas@gmail.com>
2024-08-06 13:00:24 +0300

    create apiprovider

---
+++ src/api/apiProvider.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+const http = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+http.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+http.interceptors.response.use(
+  async (response: any) => {
+    if (response.data?.data.token) {
+      localStorage.setItem("token", response?.data.data.token);
+    }
+
+    return response;
+  },
+  async (error) => {
+    const statusCode = error.response ? error.response.status : null;
+    const detailMessage = error.response;
+    console.log("In Response error", error);
+    console.log("Error Status", statusCode);
+  }
+);
+
+export default http;
