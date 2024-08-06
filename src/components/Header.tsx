2fb4ffc9f3f0e241dd65e8917135016d7c42f69f
Kaim Arvas <skaimarvas@gmail.com>
2024-08-06 23:57:38 +0300

    add images for ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <nav className="fixed flex gap-2 right-4 top-4">
-      <span className="text-[#145389] font-fortuna font-bold">kargakarga</span>
+    <nav className="flex justify-between items-center shadow-sm h-16 px-4 ">
+      <div>
+        <span className="text-[#145389] text-[20px] font-fortuna font-bold">
+          kargakarga
+        </span>
+      </div>
+      <div className="flex">
+        <Image
+          className="text-[#FFFFFF] "
+          width={25}
+          height={25}
+          src={"/bell.svg"}
+          alt="."
+        />
+        <Image width={50} height={50} src={"/25.png"} alt="." />
+      </div>
     </nav>
   );
 };
