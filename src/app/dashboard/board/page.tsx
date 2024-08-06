42b236e4fae00641f4115d3f77fcfd0fafb4ad6a
Kaim Arvas <skaimarvas@gmail.com>
2024-08-06 23:57:13 +0300

    Board blueprint

--- src/app/dashboard/board/page.tsx
+++ src/app/dashboard/board/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Page = () => {
+  return (
+    <div className="flex flex-col  items-center w-full bg-[#E4ECF9] py-10 px-8  ">
+      <div className="flex justify-between w-full">
+        {" "}
+        <span>Board Name</span>
+        <span>Filter</span>{" "}
+      </div>
+      <div>Board List</div>
+      <div>Board</div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
