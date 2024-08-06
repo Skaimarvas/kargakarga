f5926af09d7a36ed1ad870fa8db79203e0657f2d
Kaim Arvas <skaimarvas@gmail.com>
2024-08-06 13:03:09 +0300

    create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="fixed flex gap-2 right-4 top-4">
+      <span className="text-[#145389] font-fortuna font-bold">kargakarga</span>
+    </nav>
+  );
+};
+
+export default Header;
