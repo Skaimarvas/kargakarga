590ea959cd150a5abbbb666157e0e5c6f07539bc
Kaim Arvas <skaimarvas@gmail.com>
2024-08-07 08:23:42 +0300

    title navigationtabs and board created

--- src/app/dashboard/board/page.tsx
+++ src/app/dashboard/board/page.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import Board from "@/components/Board";
+import NavigationTabs from "@/components/NavigationTabs";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div className="flex flex-col  items-center w-full bg-[#E4ECF9] py-10 px-8  ">
+    <div className="flex flex-col gap-3 items-center w-full bg-[#E4ECF9] py-10 px-8  ">
       <div className="flex justify-between w-full">
         {" "}
-        <span>Board Name</span>
-        <span>Filter</span>{" "}
+        <h3 className="text-[#145389] font-semibold text-[22px] ">
+          Frontend Case
+        </h3>
+        <Image width={30} height={30} src={`/filter.svg`} alt="filter" />
       </div>
-      <div>Board List</div>
-      <div>Board</div>
+      <div className="flex w-full">
+        <NavigationTabs />
+      </div>
+      <Board />
     </div>
   );
 };
