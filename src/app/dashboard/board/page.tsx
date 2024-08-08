83b5bc1c19b6120516bc54b108c35cbaeeaa2b2e
Kaim Arvas <skaimarvas@gmail.com>
2024-08-08 07:49:13 +0300

    add state , token condition

--- src/app/dashboard/board/page.tsx
+++ src/app/dashboard/board/page.tsx
@@ -1,10 +1,36 @@
+"use client";
 import Board from "@/components/Board";
 import NavigationTabs from "@/components/NavigationTabs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const Page = () => {
+const BoardPage = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen w-full">
+        <span>Yükleniyor</span>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col gap-3 items-center w-full bg-[#E4ECF9] py-10 px-8  ">
+    <div className="flex flex-col gap-3 items-center w-full bg-[#E4ECF9] h-full py-5 px-8  ">
       <div className="flex justify-between w-full">
         {" "}
         <h3 className="text-[#145389] font-semibold text-[22px] ">
@@ -15,9 +41,10 @@ const Page = () => {
       <div className="flex w-full">
         <NavigationTabs />
       </div>
+
       <Board />
     </div>
   );
 };
 
-export default Page;
+export default BoardPage;
